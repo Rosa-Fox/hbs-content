253bef1b3c26a03b2f1fb46ef8d33eb5aece0626
Najaf Ali <ali.najaf@gmail.com>
2015-07-20 23:12:49 +0100

    Adds Person.for_website_homepage

--- lib/hbs_content/contentful_data_provider.rb
+++ lib/hbs_content/contentful_data_provider.rb
@@ -5,6 +5,7 @@ require_relative 'person'
 module HbsContent
   class ContentfulDataProvider
     CONTENT_TYPE_IDS = { person: '6NsnpIaZEssuUCwysAacM4' }
+    ENTRY_IDS = { homepage_team_list: '2ASQ8Zlw5WM6waOGO4284y' }
 
     def initialize(options = {})
       @client = Contentful::Client.new(options)
@@ -12,6 +13,14 @@ module HbsContent
     
     def all(type)
       entries = @client.entries('content_type' => CONTENT_TYPE_IDS[type])
+      entries.map do |entry|
+        convert(type, entry)
+      end
+    end
+
+    def people_for_homepage_team_list
+      entries = @client.entry(ENTRY_IDS[:homepage_team_list]).fields[:people].map(&:resolve)
+      type    = :person
       entries.map do |entry|
         convert(type, entry)
       end
@@ -29,11 +38,22 @@ module HbsContent
           p.email             = fields[:email]
           p.short_bio         = fields[:shortBio]
           p.long_bio          = fields[:longBio]
-          p.profile_photo_url = ContentfulImageUrl.new(fields[:profilePhoto].first.fields[:file].properties[:url])
           p.skype_id          = fields[:skypeId]
           p.linkedin_url      = fields[:linkedInUrl]
           p.github_url        = fields[:githubUrl]
           p.twitter_url       = fields[:twitterUrl]
+
+          # Resolve the profile photo URL if required
+          profile_photo_field = fields[:profilePhoto].first
+
+          case profile_photo_field.type
+          when 'Link'
+            profile_photo_field = profile_photo_field.resolve
+          when 'Asset'
+            # do nothing
+          end
+
+          p.profile_photo_url = ContentfulImageUrl.new(profile_photo_field.fields[:file].properties[:url])
         end
       end
     end
--- lib/hbs_content/person.rb
+++ lib/hbs_content/person.rb
@@ -4,13 +4,20 @@ module HbsContent
                   :email, :short_bio, :long_bio, :profile_photo_url,
                   :skype_id, :linkedin_url, :github_url, :twitter_url
 
-    def self.all
-      provider.all(:person)
+    class << self
+      def all
+        provider.all(:person)
+      end
+
+      def for_website_homepage
+        provider.people_for_homepage_team_list
+      end
+
+      private
+      def provider
+        HbsContent.provider
+      end
     end
 
-    private
-    def self.provider
-      HbsContent.provider
-    end
   end
 end
